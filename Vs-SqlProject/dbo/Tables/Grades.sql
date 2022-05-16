efee656cbbc902382fc099591ca1db335899b46d
Vincent Bourgeois <vincent.bourgeois@bouyguestelecom.fr>
2022-05-16 17:32:55 +0200

    Added solution for product grades.

---
+++ Vs-SqlProject/dbo/Tables/Grades.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Grades]
+(
+	[ProductId] INT NOT NULL,
+	[CustomerId] INT NOT NULL,
+	[Grade] INT NOT NULL, 
+    CONSTRAINT [PK_Grades] PRIMARY KEY ([CustomerId], [ProductId]),
+	CONSTRAINT [FK_Grades_Products] FOREIGN KEY ([ProductId]) REFERENCES [dbo].[Products] ([ProductId]),
+	CONSTRAINT [FK_Grades_Customers] FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customers] ([CustomerId])
+)
